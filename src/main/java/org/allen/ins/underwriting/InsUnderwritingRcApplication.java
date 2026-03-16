4cb8c4db5e78829c6acefef2e172389b476ac315
Victoria-Spencer <1557573258@qq.com>
2026-03-16 19:11:48 +0800

    基本框架搭建

---
+++ src/main/java/org/allen/ins/underwriting/InsUnderwritingRcApplication.java
@@ -0,0 +1,13 @@
+package org.allen.ins.underwriting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsUnderwritingRcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsUnderwritingRcApplication.class, args);
+    }
+
+}
