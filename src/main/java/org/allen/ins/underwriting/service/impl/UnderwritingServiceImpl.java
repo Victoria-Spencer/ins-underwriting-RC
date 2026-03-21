10e39ecfbbcfb21f6ccd960e4fad6e178efb5937
Victoria-Spencer <1557573258@qq.com>
2026-03-21 15:31:14 +0800

    已解决：mp和spring-boot-starter底层依赖冲突

--- src/main/java/org/allen/ins/underwriting/service/impl/UnderwritingServiceImpl.java
+++ src/main/java/org/allen/ins/underwriting/service/impl/UnderwritingServiceImpl.java
@@ -3,7 +3,9 @@ package org.allen.ins.underwriting.service.impl;
 import org.allen.ins.underwriting.pojo.dto.UnderwritingRequestDTO;
 import org.allen.ins.underwriting.pojo.vo.UnderwritingResponseVO;
 import org.allen.ins.underwriting.service.UnderwritingService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UnderwritingServiceImpl implements UnderwritingService {
     @Override
     public UnderwritingResponseVO calculateUnderwritingRisk(UnderwritingRequestDTO request) {
